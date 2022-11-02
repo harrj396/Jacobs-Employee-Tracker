9c43a6f7e4cc41037da9a88f10444dd451d76415
harrj396 <ahletmethink@gmail.com>
2022-11-02 17:20:36 -0400

    started creating tables for employees

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,12 @@
-DROP DATABASE IF EXISTS movies_db;
-CREATE DATABASE movies_db;
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
 
-USE movies_db;
+USE employee_db;
 
-CREATE TABLE movies (
+CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  movie_name VARCHAR(100) NOT NULL
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+  name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE reviews (
