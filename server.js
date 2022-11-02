9ad99c400e979a226a1e00e7bb1a47fcbdd80a40
harrj396 <ahletmethink@gmail.com>
2022-11-02 18:58:55 -0400

    Added first question for node

--- server.js
+++ server.js
@@ -22,3 +22,10 @@ const db = mysql.createConnection(
   console.log(`Connected to the employee_db database.`)
 );
 
+const firstQuestion = [ 
+  { type: "list",
+  message: "Please select an option",
+  choice: ["View All Employees", "Add Employee", "View All Roles", "Add Role", "View All Departments", "Add Department"],
+  name: "landingPage"
+}
+]
