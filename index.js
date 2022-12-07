4ba5e23251de318389bddb04bce18d2058ed6580
harrj396 <ahletmethink@gmail.com>
2022-12-07 04:01:20 -0500

    switching to adding and removing departments

--- index.js
+++ index.js
@@ -25,58 +25,41 @@ const firstQuestion = [
   choices: ["View All Employees", "Add Employee", "View All Roles", "Add Role", "View All Departments", "Add Department"],
   name: "landingPage"
 },
-  console.log('you made it this'),
   (function (response) {
   switch(response.action) {
     case 'View All Employees' : employeeView();
       break
 
-    // case 'Add Employee' : employeeAdd();
-    //   break;
-  // this currently breaks node index,js for these functions being unknown 
-    // case 'View All Roles' : roleView();
-    //   break;
+    case 'Add Employee' : employeeAdd();
+       break;
+
+    case 'View All Roles' : roleView();
+      break;
     
-    // case '"Add Role' : roleAdd();
-    //   break
+    case '"Add Role' : roleAdd();
+      break
 
-    // case 'View All Departments' : departmentView();
-    //   break;
+    case 'View All Departments' : departmentView();
+      break;
 
-    // case 'Add Department' : departmentAdd();
-    //   break;
+    case 'Add Department' : departmentAdd();
+      break;
   }
 }),
 
 
-function employeeView() {
-  db.query(`SELECT * FROM employee`, function(err, data){
+
+
+function departmentView() {
+  db.query(`SELECT * FROM department`, function(err, data){
     if (err) throw err;
-    console.log('Every employee');
+    console.log('Every department');
     console.table(data);
     firstQuestion()
   })
 },
 
-function employeeAdd() {
-  inquirer
-  .prompt([
-    {
-      message: 'Enter the employees first name',
-      name: 'firstName'
-    },
 
-    {
-      message: 'Enter the employees last name',
-      name: 'lastName'
-    }
-  ])
-
-  // .then((function (response) {
-  //   db.query(`INSERT INTO employee (firstName, lastName) VALUES (`${response.first_name}`, `${response.last_name}`, response, function )
-  //   )
-  // }))
-}
 ]
 function init() {
   inquirer
